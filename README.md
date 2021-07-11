306a2dde4dec89ef495fee895755ff691aacbe35
kapuragam <kapur.agam@gmail.com>
2021-07-11 23:40:21 +0100

    1. Initial Commit 2. Data collection for a  basic e2e ML project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # everyday-python
-A place to write in python daily and catalogue all the interesting, quirky things I've come across while developing in python
+Daily python code and catalogued cheatsheets for Pandas, NumPy, SciPy, Keras, Tensorflow, Numba etc
---
+++ src/e2e-ml-project/DataCollector.py
@@ -0,0 +1,50 @@
+import os
+import tarfile
+import urllib.request
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = "../../data/housing"
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing" + "/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+def main():
+    fetch_housing_data()
+    housing = load_housing_data()
+    print(housing.head())
+    print(housing.info())
+    print(housing.describe())
+    # housing.hist(bins=50, figsize=(20, 15))
+    # plt.show()
+    train_set, test_set = split_train_test(housing, 0.2)
+    print(len(train_set), "train +", len(test_set), "test")
+
+
+if __name__ == '__main__':
+    main()
